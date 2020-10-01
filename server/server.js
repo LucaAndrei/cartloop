23d27c535e02cce1a071b3a2351b0ea0c894bae5
Andrei Luca <andrei.x.luca@kp.org>
2020-10-01 23:43:00 +0300

    update server routes

--- server/server.js
+++ server/server.js
@@ -5,20 +5,21 @@ const messages = require('./routes/messages');
 const app = express();
 
 // Port that the webserver listens to
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-//Setup Error Handlers
-const errorHandlers = require("./handlers/errorHandlers");
-app.use(errorHandlers.notFound);
-if (process.env.ENV === "DEVELOPMENT") {
-  app.use(errorHandlers.developmentErrors);
-}
 
 app.use('/api/messages', messages);
+
+//Setup Error Handlers
+const errorHandlers = require('./handlers/errorHandlers');
+app.use(errorHandlers.notFound);
+if (process.env.ENV === 'DEVELOPMENT') {
+  app.use(errorHandlers.developmentErrors);
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);

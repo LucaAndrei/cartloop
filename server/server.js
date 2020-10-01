dcde5bbb2858de49ee8fa8ca7ba01ee298603d0c
Andrei Luca <andrei.x.luca@kp.org>
2020-10-01 10:45:16 +0300

    initial project setup

---
+++ server/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const messages = require('./routes/messages');
+
+const app = express();
+
+// Port that the webserver listens to
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+//Setup Error Handlers
+const errorHandlers = require("./handlers/errorHandlers");
+app.use(errorHandlers.notFound);
+if (process.env.ENV === "DEVELOPMENT") {
+  app.use(errorHandlers.developmentErrors);
+} else {
+  app.use(errorHandlers.productionErrors);
+}
+
+app.use('/api/messages', messages);
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+module.exports = app;
